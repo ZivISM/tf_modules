32346856be2da9bcfdf4804093d2f0c671ef2051
Ziv Ismailov <zivismailov@gmail.com>
2025-02-17 01:06:20 +0200

    updated dependencies

--- aws_transfer_sftp/modules/transfer_server.tf
+++ aws_transfer_sftp/modules/transfer_server.tf
@@ -24,8 +24,6 @@ resource "aws_transfer_server" "sftp" {
     Name    = var.transfer_server_name
     Project = var.project
   }
-
-  depends_on = [aws_route53_zone.main]
 }
 
 ###############################################################################
